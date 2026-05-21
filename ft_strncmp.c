990bf77774e81d7f40b868a450484b87c3879f71
Diego-Matoss <diegoeduardomatos@gmail.com>
2026-05-21 19:10:42 +0200

    ft_strncmp added

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,14 +14,13 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	unsigned char *str1;
-	unsigned char *str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	
 	while (i < n && (s1[i] || s2[i]))
 	{
 		if (str1[i] != str2[i])
