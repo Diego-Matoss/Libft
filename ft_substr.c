21ba53682715b60f5485b55cfed78c5fc0462b1b
Diego Matos <diegoeduardomatos@gmail.com>
2026-05-26 20:24:31 +0200

    ft_substr.c added

---
+++ ft_substr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dimatos- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 19:38:05 by dimatos-          #+#    #+#             */
+/*   Updated: 2026/05/26 19:38:06 by dimatos-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	str_length;
+	size_t	sub_length;
+	char	*str;
+
+	if (s == NULL)
+		return (NULL);
+	str_length = ft_strlen(s);
+	if (start >= str_length)
+		sub_length = 0;
+	else if (len > str_length - start)
+		sub_length = str_length - start;
+	else
+		sub_length = len;
+	str = malloc(sizeof(char) * (sub_length + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s + start, sub_length + 1);
+	return (str);
+}
+
+/* #include <stdio.h>
+int main(void)
+{
+	char string[] = "Hola mundo";
+	char *substr = ft_substr(string, 0, 3);
+	printf("%s\n", substr);
+	return (0);
+} */
