6235c2c467d8d8f3bc1c9e9eeca4de5513a4e704
Diego-Matoss <diegoeduardomatos@gmail.com>
2026-06-03 16:32:58 +0200

    ft_calloc.c fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (nmemb != 0 && size > 18446744073709551615UL / nmemb)
+	if (nmemb != 0 && size > (size_t)(-1) / nmemb)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (!ptr)
